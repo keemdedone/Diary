e5ce8ac4c17f980ed5eca35780bf6e77579ae9f9
keemdedone <komchan447@gmail.com>
2023-03-13 18:45:26 +0700

    change month complete

--- src/app/pages/calendar/calendar-main/calendar-main.component.ts
+++ src/app/pages/calendar/calendar-main/calendar-main.component.ts
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalendarMainComponent implements OnInit {
   dayName = ['Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun'];
+  monthName = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
   days: boolean[] = [];
   daysBefore: string[] = [];
   daysAfter: string[] = [];
@@ -15,7 +29,7 @@ export class CalendarMainComponent implements OnInit {
   selectYear: number;
 
   constructor() {
-    this.selectMonth = new Date().getMonth() + 1;
+    this.selectMonth = new Date().getMonth();
     this.selectYear = new Date().getFullYear();
   }
 
@@ -26,26 +40,52 @@ export class CalendarMainComponent implements OnInit {
   getDays() {
     this.getBeforeDays();
     this.getAfterDays();
-    const days = new Date(this.selectYear, this.selectMonth, 0).getDate();
+    const days = new Date(this.selectYear, this.selectMonth + 1, 0).getDate();
     this.days = new Array(days).fill(false);
   }
 
   getBeforeDays() {
-    const beforeMonth = new Date(
-      this.selectYear,
-      this.selectMonth - 1,
-      0
-    ).getDay();
+    const beforeMonth = new Date(this.selectYear, this.selectMonth, 0).getDay();
     const daysBefore =
       beforeMonth != 0
         ? new Array(beforeMonth).fill('')
-        : new Array(6).fill('');
+        : new Array(0).fill('');
     this.daysBefore = daysBefore;
   }
 
   getAfterDays() {
-    const afterMonth = new Date(this.selectYear, this.selectMonth, 0).getDay();
+    const afterMonth = new Date(
+      this.selectYear,
+      this.selectMonth + 1,
+      0
+    ).getDay();
     const daysAfter = afterMonth != 0 ? new Array(7 - afterMonth).fill('') : [];
     this.daysAfter = daysAfter;
+  }
+
+  onCurrentDate() {
+    this.selectMonth = new Date().getMonth();
+    this.selectYear = new Date().getFullYear();
+    this.getDays();
+  }
+
+  onNextMonth() {
+    if (this.selectMonth == 11) {
+      this.selectMonth = 0;
+      this.selectYear += 1;
+    } else {
+      this.selectMonth += 1;
+    }
+    this.getDays();
+  }
+
+  onPreviousMonth() {
+    if (this.selectMonth == 0) {
+      this.selectMonth = 11;
+      this.selectYear -= 1;
+    } else {
+      this.selectMonth -= 1;
+    }
+    this.getDays();
   }
 }
--- src/app/pages/calendar/calendar.module.ts
+++ src/app/pages/calendar/calendar.module.ts
@@ -3,15 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { CalendarRoutingModule } from './calendar-routing.module';
 import { CalendarMainComponent } from './calendar-main/calendar-main.component';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  declarations: [
-    CalendarMainComponent
-  ],
+  declarations: [CalendarMainComponent],
   imports: [
     CommonModule,
-    CalendarRoutingModule
-  ]
+    CalendarRoutingModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
 })
-export class CalendarModule { }
+export class CalendarModule {}
